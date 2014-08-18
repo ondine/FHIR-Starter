a6e5aa30992e0da9ff351bc12e99c7921ce5369e
Peter Bernhardt <peter.bernhardt@gmail.com>
2014-08-18 09:28:11 -0700

    Updates to Practitioner edit form

--- src/client/app/administration/practitioner/practitionerDetail.js
+++ src/client/app/administration/practitioner/practitionerDetail.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2014 Peter Bernhardt, et. al.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 (function () {
     'use strict';
 
@@ -133,12 +147,12 @@
 
             function intitializeRelatedData(data) {
                 vm.practitioner = data;
-                humanNameService.init(vm.practitioner.name);
+                humanNameService.init(vm.practitioner.name, 'single');
                 demographicsService.init(vm.practitioner.gender, vm.practitioner.maritalStatus);
                 demographicsService.setBirthDate(vm.practitioner.birthDate);
                 attachmentService.init(vm.practitioner.photo, "Photos");
                 identifierService.init(vm.practitioner.identifier);
-                addressService.init(vm.practitioner.address, true);
+                addressService.init(vm.practitioner.address, true, 'single');
                 telecomService.init(vm.practitioner.telecom, true, true);
                 vm.practitioner.fullName = humanNameService.getFullName();
                 if (vm.practitioner.organization && vm.practitioner.organization.reference) {
--- src/client/app/shared/humanName/humanNameService.js
+++ src/client/app/shared/humanName/humanNameService.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2014 Peter Bernhardt, et. al.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 (function () {
     'use strict';
 
@@ -7,12 +21,14 @@
 
     function humanNameService(common) {
         var humanNames = [];
+        var _mode = 'multi';
 
         var service = {
             add: add,
             remove: remove,
             getAll: getAll,
             getFullName: getFullName,
+            getMode: getMode,
             init: init,
             mapFromViewModel: mapFromViewModel,
             reset: reset
@@ -52,7 +68,12 @@
             return -1;
         }
 
-        function init(items) {
+        function getMode() {
+            return _mode;
+        }
+
+        function init(items, mode) {
+            _mode = mode ? mode : 'multi';
             if (angular.isArray(items)) {
                 humanNames = [];
                 _.forEach(items, function (item) {
